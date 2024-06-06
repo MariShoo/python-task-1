5e80592c2cc9b48f1963818bdad0c24648000f26
MariShoo <151842927+MariShoo@users.noreply.github.com>
2024-06-07 01:21:28 +0400

    main.py

--- task1-python/main.py
+++ task1-python/main.py
@@ -1,1 +1,27 @@
 print (" hello world")
+
+x = 2
+print (x) # prints x as int type 
+print (float(x)) # prints x as float 
+
+b= "first task"
+print(len(b)) # prints length of b variable (10)
+
+print("task" in b) # checks if word "task" is in given string b 
+print("python" not in b) # checks if word "python" isn't in given string b
+
+# string modifications 
+
+print(b.upper()) #prints b variable's value in upper cases 
+print(b.lower()) #prints b variable's value in lower cases
+print(b.replace("first" , "second")) # changes word "first" to "second"
+print(b.split("_")) # splits string with _
+
+# concatenation 
+
+a= "run"
+print (a + " " + b) #prints a and b string values 
+
+
+
+
